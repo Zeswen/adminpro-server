1cc495a5feda3fc5dbf91533f5cc707f0100afb0
BuildTools <unconfigured@null.spigotmc.org>
2019-10-02 12:54:25 +0200

    register and login

--- handlers/users.js
+++ handlers/users.js
@@ -5,6 +5,47 @@ import { jsonRes } from '../config/constants';
 const USERS = 'users';
 const saltRounds = 10;
 
+export const register = async (req, res) => {
+  try {
+    const { name, email, password, img } = req.body;
+    const db = await dbPool.connect();
+    const usersCollection = db.collection(USERS);
+    await usersCollection.createIndex({ email: 1 }, { unique: true });
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const userCreated = await usersCollection.insertOne({
+      name,
+      email,
+      password: hashedPassword,
+      img,
+      role: 'user'
+    });
+    await dbPool.disconnect();
+    jsonRes(res, 200, userCreated);
+  } catch (err) {
+    console.error(err);
+    jsonRes(res, 500, null, err);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const db = await dbPool.connect();
+    const usersCollection = db.collection(USERS);
+    const userFound = await usersCollection.findOne({ email });
+    const matchPassword = await bcrypt.compare(password, userFound.password);
+    await dbPool.disconnect();
+    if (matchPassword) {
+      jsonRes(res, 200, userFound);
+    } else {
+      jsonRes(res, 401, null, "Password doesn't match.");
+    }
+  } catch (err) {
+    console.error(err);
+    jsonRes(res, 500, null, err);
+  }
+};
+
 export const insertUser = async (req, res) => {
   try {
     const { name, email, password, img, role } = req.body;
@@ -12,15 +53,15 @@ export const insertUser = async (req, res) => {
     const usersCollection = db.collection(USERS);
     await usersCollection.createIndex({ email: 1 }, { unique: true });
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-    const customerCreated = await usersCollection.insertOne({
+    const userCreated = await usersCollection.insertOne({
       name,
       email,
       password: hashedPassword,
       img,
       role
     });
-    jsonRes(res, 200, customerCreated);
     await dbPool.disconnect();
+    jsonRes(res, 200, userCreated);
   } catch (err) {
     console.error(err);
     jsonRes(res, 500, null, err);
@@ -32,8 +73,8 @@ export const queryUsers = async (_, res) => {
     const db = await dbPool.connect();
     const usersCollection = db.collection(USERS);
     const users = await usersCollection.find({}).toArray();
-    jsonRes(res, 200, users);
     await dbPool.disconnect();
+    jsonRes(res, 200, users);
   } catch (err) {
     console.error(err);
     jsonRes(res, 500, null, err);
